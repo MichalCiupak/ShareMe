16c2543a83e87fcf8e5880a5ba124640b8882c00
Michal Ciupak <michal.ciupak0@gmail.com>
2023-10-18 20:56:18 +0200

    created pin adding functionality

--- shareme_frontend/src/components/CreatePin.jsx
+++ shareme_frontend/src/components/CreatePin.jsx
@@ -1,9 +1,180 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
-const CreatePin = () => {
+import { client } from '../client';
+import Spinner from './Spinner';
+import { categories } from '../utils/data';
+//categories
+
+
+const CreatePin = ({user}) => {
+
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [destination, setDestination] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(false);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+  const uploadImage = (e) => {
+    const {type, name} = e.target.files[0];
+
+    if(type === 'image/png' || type === 'image/svg' || type === 'image/jpeg' || type === 'image/gif' || type === 'image/tiff'){
+      setWrongImageType(false);
+      setLoading(true);
+      client.assets
+        .upload('image', e.target.files[0], {contentType: type, filename: name})
+        .then((document) => {
+          setImageAsset(document);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log('image upload error', error)
+        })
+      } else {
+      setWrongImageType(true);
+    }
+  }
+
+  const savePin = () => {
+    if(title && about && destination && imageAsset?._id && category){
+      const doc = {
+        _type: 'pin',
+        title,
+        about,
+        destination,
+        image: {
+          _type: 'image',
+          asset: {
+            _type: 'reference',
+            _ref: imageAsset?._id
+          }
+        },
+        userId: user._id,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user._id
+        },
+        category
+      }
+      client.create(doc)
+        .then(() => {
+          navigate('/')
+        })
+    } else {
+      setFields(true);
+      window.scrollTo(0,0);
+      setTimeout(() => setFields(false), 2000);
+      
+    }
+  }
+
+
+
   return (
-    <div>
-      CreatePin
+    <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
+      {fields && (
+        <p className='text-red-500 mb-5 text-xl transition-all duration-150 ease-in'> 
+          Please fill in all the fields.
+        </p>
+      )}
+      <div className='flex lg:flex-row flex-col justify-left items-left bg-white lg:p-5 p-3 lg:w-4/5 w-full'>
+        <div className='bg-secondaryColor bg-red-500 p-3 flex flex-0.7 w-full'>
+          <div className='flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420'>
+            {loading && <Spinner/>}
+            {wrongImageType && <p>Wrong image type</p>}
+            {!imageAsset ? (
+              <lebel >
+                <div className='flex flex-col items-center justify-center h-full'>
+                  <div className='flex flex-col justify-center items-center'>
+                    <p className='font-bold tet-2xl'>
+                      <AiOutlineCloudUpload/>
+                    </p>
+                    <p className='text-lg'>Click to upload</p>
+
+                  </div>
+                  <p className='mt-32 text-gray-400'>
+                    Recommendation: use high-quality JPG, SVG, PNG, GIF files.
+                  </p>
+                </div>
+                <input 
+                type='file'
+                name='upload-image'
+                onChange={uploadImage}
+                className=''
+                />
+
+      
+              </lebel>
+            ) : (
+              <div className='relative h-full'>
+                <img src={imageAsset?.url} alt='uploaded-pic' className='h-full w-full'/>
+                <button type='button' 
+                className='absolute bottom-3 right-3 p-3 rounded-full bg0white text-xl cursoer-pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out'
+                onClick={() => setImageAsset(null)}
+                >
+                  <MdDelete/>
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className='flex flex-1 flex-col gap-6 lg:pl-5 mt-5'>
+            <input  type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder='Add your title here'
+            className='outline-none text-xl sm:text-xl font-bold border-b-2 border-gray-200 p-2'
+            />
+            {user && (
+              <div className='flex gap-2 my-8 items-center justify-left bg-white rounded-lg'>
+                <img src={user.image}
+                className='w-10 h-10 rounded-full'
+                alt='user-profile'
+                />
+                <p className='font-bold'>{user.userName}</p>
+              </div>
+            )}
+            <input  type='text'
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder='What is your pin about?'
+            className='outline-none my-8 text-xl sm:text-xl font-bold border-b-2 border-gray-200 p-2'
+            />
+            <input  type='text'
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder='Add a destination link'
+            className='outline-none my-8 text-xl sm:text-xl font-bold border-b-2 border-gray-200 p-2'
+            />
+            <div className='flex flex-col'>
+              <div>
+                <p className='mb-2 my-8 font-semibold text-lg sm:text-xl'>
+                  Choose Pin Category
+                </p>
+                <select onChange={(e) => setCategory(e.target.value)}
+                className='appearance-none text-gray-700 outline-none w-3/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer'>
+                  <option value="other" className='bg-white'>Select category</option>
+                  {categories.map((category) => (
+                    <option className='font-bold border-0 outline-none capitalize bg-white text-gray-700'
+                    value={category.name}>{category.name}</option>
+                  ))}
+                </select>
+              </div>
+              <div className='flex justify-end items-end mt-5'>
+                <button type='button' onClick={savePin}
+                className='bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none'>
+                  Save Pin
+                </button>
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -6,13 +6,43 @@ import { client, urlFor } from '../client';
 import { MdDownloadForOffline } from 'react-icons/md';
 import { AiTwotoneDelete } from 'react-icons/ai';
 import { BsFillArrowUpRightCircleFill} from 'react-icons/bs'
+import { fetchUser } from '../utils/fetchUser';
 
-const Pin = ({pin: {postedBy, image, _id, destination}}) => {
+const Pin = ({pin: {postedBy, image, _id, destination, save}}) => {
 
   const [postHovered, setPostHovered] = useState(false)
-  const [savingPost, setSavingPost] = useState(false)
+  
   const navigate = useNavigate();
+  const user = fetchUser();
 
+  const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.googleId))?.length;
+  const savePin = (id) => {
+    if(!alreadySaved){
+
+      client.patch(id).setIfMissing({save: []})
+      .insert('after', 'save[-1]', [{
+        _key: uuidv4(),
+        userId: user.googleId,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user.googleId
+        }
+      }])
+      .commit()
+      .then(() => {
+        window.location.reload();
+
+      });
+    }
+  }
+
+  const deletePin = (id) => {
+    client
+      .delete(id)
+      .then(() => {
+        window.location.reload();
+      })
+  }
   return (
     <div className='m-2'>
       <div 
@@ -35,11 +65,57 @@ const Pin = ({pin: {postedBy, image, _id, destination}}) => {
                 </a>
 
               </div>
+              {alreadySaved ? (
+                <button type='button' className='bg-white opacity-70 hover:opacity-100 text-black font-bold px-5 py-1 text-base rounded-full hover:shadow-md outlined-none'>
+                  {save?.length} Saved
+                </button>
+              ): (
+                <button onClick={(e) => {
+                  e.stopPropagation();
+                  savePin(_id);
+                }}
+                type='button' className='bg-white opacity-70 hover:opacity-100 text-black font-bold px-5 py-1 text-base rounded-full hover:shadow-md outlined-none'>
+                  
+                  Save
+                </button>
+              )}
             </div>
-            
+            <div className='flex justify-between items-center gap-2 w-full'
+            >
+              {destination && (
+                <a href={destination}
+                target="_blank"
+                rel="noreferrer"
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                className='bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md'>
+                  <BsFillArrowUpRightCircleFill/>
+                  {destination.length > 20 ? destination.slice(8,20) : destination.slice(8)}
+                </a>
+              )}
+              {postedBy?._id === user.googleId && (
+                <button type='button'
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deletePin(_id);
+                }}
+                className='bg-white p-2 opacity-70 hover:opacity-100 text-black font-bold  py-1 text-base rounded-full hover:shadow-md outlined-none'
+                >
+                  <AiTwotoneDelete/>
+                </button>
+              )}
+            </div>
           </div>
         )}
       </div>
+      <Link to={`user-profile/${postedBy?._id}`} className='flex gap-2 mt-2 py-2 items-center justify-center rounded-full hover:scale-110'>
+        <img className='w-8 h-8 rounded-full object-cover'
+        src={postedBy?.image}
+        alt='user-profile'
+        />
+        <p className='font-semibold capitalize'>{postedBy?.userName}</p>
+      </Link>
     </div>
   )
 }
